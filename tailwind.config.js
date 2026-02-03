24160b03ef83f3eb786b98e3b63d332e5e71ce13
Nattapart Worakun <earthondev@gmail.com>
2026-02-04 01:50:17 +0700

    Replace Tailwind CDN with local build

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./*.html", "./**/*.js"],
+  theme: {
+    extend: {
+      fontFamily: {
+        heading: ['Space Grotesk', 'sans-serif'],
+        body: ['Sarabun', 'sans-serif'],
+        mono: ['JetBrains Mono', 'monospace'],
+      }
+    }
+  },
+  plugins: [],
+};
